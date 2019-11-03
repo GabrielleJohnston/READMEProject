c00702436f16c4521f3bca78f6e2d027dbee5d99
MarcellaSwanto <mai1817@ic.ac.uk>
2019-11-03 12:37:42 +0000

    ''

--- octaveSmooth_PointReduced.m
+++ octaveSmooth_PointReduced.m
@@ -455,7 +455,7 @@ fd = 10^0.05;
 fupper10 = fcenter10 * fd;
 flower10 = fcenter10 / fd;
 
-%   Third Octave Smoothing the signal
+%   Third Octave Smoothing the downsampled signal
 for i=1:length(fcenter10)
     mag_smoothed_bark(i) =  sum(amp(find(f >= flower10(i) & f <= fupper10(i))))/length(find(f >= flower10(i) & f <= fupper10(i)));
 end
@@ -467,7 +467,7 @@ plot(fcenter10, mag_smoothed_bark)
 set(gca, 'XScale', 'log')
 grid on
 ylim([-100 50])
-title('Smoothed magnitude spectrum')
+title('Third octave smoothed magnitude spectrum of the downsampled signal')
 xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
 xlim([10 16000])
@@ -481,7 +481,7 @@ xlim([10 16000])
 %fft of the original signal
 mNfft = 2^nextpow2(length(mario));
 mdft = fft(mario, mNfft);
-fft_mario = 20*log(abs(mdft)); % fft of original signal
+fft_mario = (abs(mdft)); % fft of original signal
 f_mario = fs_mario*((0:mNfft-1)/mNfft);
 
 
@@ -493,29 +493,8 @@ end
 %   Convolving the impulse reponse smoothed in the frequency domain
 %   with the test acoustic signal (Mario)
 %   mag_smoothed_bark is the point reduced and octave smoothed IR
-conv1 = conv(oct_smoothed_mario, mag_smoothed_bark);
 
-ifft_IR = ifft(mag_smoothed_bark);
-conv2 = conv( conv1,ifft_IR);
+temp = oct_smoothed_mario.* mag_smoothed_bark.* ifft(mag_smoothed_bark);
 
-deconv = abs(ifft(conv2));
+deconv = abs(ifft(temp));
 sound(deconv, fs_mario)
-
-%   Plotting fft of mario and the smoothed fft
-figure(9)
-hold on
-plot(fft_mario, 'r')
-plot(fcenter10, oct_smoothed_mario, 'b')
-legend('FFT of noisy Mario signal', 'Third Octave Smoothed Mario signal');
-set(gca, 'XScale', 'log')
-title('Smoothed magnitude spectrum of the input signal')
-xlabel('Frequency (Hz)')
-ylabel('Magnitude (dB)')
-xlim([10 16000])
-
-figure(10)
-plot(fcenter10, output(1:32))
-title('Convolution Output')
-xlabel('Frequency (Hz)')
-ylabel('Magnitude (dB)')
-set(gca, 'XScale', 'log')
