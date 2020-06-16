8952be38d4dac196e0721322b27922b9384748b2
MarcellaSwanto <mai1817@ic.ac.uk>
2020-06-16 11:38:00 +0100

    Added true octave smooth to mono page

--- AppConversion/plotspectlogf_modified.m
+++ AppConversion/plotspectlogf_modified.m
@@ -84,51 +84,9 @@ end
 [m,lim3] = min(abs(freq-50));
 [m,lim4] = min(abs(freq-200));
 
-% figure(3);
-
-%If there is no smoothing
-% if(smoothing == 0)
-%     semilogx(freq, db);
-%     xlim([f1 f2+(0.1*f2)]);
-%     ylim([-100 50]);
-%     if (size(x,2) == 1)
-%           title('Frequency Response');
-%     elseif (i == 1)
-%           title('RTF Left Channel');
-%     else
-%           title('RTF Right Channel');
-%     end
-%     xlabel('Frequency, Hz');
-%     ylabel('Amplitude, dB');
-%     grid on;
-% elseif(smoothing == 1) %Base 2 1/3 Octave Smooth
-%     [k2, fcenter2] = OctaveSmooth(db, freq, 2);
-%     semilogx(fcenter2,k2);
-%     xlim([f1 f2+(0.1*f2)*0.50]);
-%     ylim([-100 50]);
-%     xlabel('Frequency, Hz');
-%     ylabel('Amplitude, dB');
-%     grid on;
-%     title('Base 2 1/3 Octave Smoothing');
-% elseif(smoothing == 2) %Base 10 1/3 Octave Smooth
-    [k10, fcenter10] = OctaveSmooth(db, freq, 10);
-%     semilogx(fcenter10,k10);
-%     xlim([f1 f2+(0.1*f2)*0.50]);
-%     ylim([-100 50]);
-%     xlabel('Frequency, Hz');
-%     ylabel('Amplitude, dB');
-%     grid on;
-%     title('Base 10 1/3 Octave Smoothing');
-% else %Bark Smoothing
-    [mag_bark, freq_bark] = rlogbark_short(freq, db);
-%     semilogx(freq_bark, mag_bark);
-%     xlim([f1 f2+(0.1*f2)*0.50]);
-%     ylim([-100 50]);
-%     xlabel('Frequency, Hz');
-%     ylabel('Amplitude, dB');
-%     grid on;
-%     title('Bark Smoothing');
-% end
+[k10, fcenter10] = OctaveSmooth(db, freq, 10); % 1/3 Base 10 Octave smooth
+[mag_bark, freq_bark] = rlogbark_short(freq, db); % Bark smoothing
+[trueOct, trueOct_f]=trueOctave(db, freq); % True octave Base 10 smooth
 
 if strcmp(mOrs,'mono-left')
     %Left
@@ -137,24 +95,27 @@ if strcmp(mOrs,'mono-left')
     [group_delay_bark_left, freq_bark] = rgeobark_short(freq, group_delay_left);
     % Shifting group delay down to 0 by the propagation delay (minimum value)
     group_delay_bark_left = group_delay_bark_left - min(group_delay_bark_left);
+    [trueOct_left, trueOct_f]=trueOctave(db_left, freq);
 elseif strcmp(mOrs,'mono-right')
       %Right
     [mag_bark_right, freq_bark] = rlogbark_short(freq, db_right);
     [phase_bark_right, freq_bark] = rgeobark_short(freq, myphase_right);
     [group_delay_bark_right, freq_bark] = rgeobark_short(freq, group_delay_right);
     group_delay_bark_right = group_delay_bark_right - min(group_delay_bark_right);
+    [trueOct_right, trueOct_f]=trueOctave(db_right, freq);
 else 
     %Stereo
     [mag_bark_left, freq_bark] = rlogbark_short(freq, db_left);
     [phase_bark_left, freq_bark] = rgeobark_short(freq, myphase_left);
     [group_delay_bark_left, freq_bark] = rgeobark_short(freq, group_delay_left);
     group_delay_bark_left = group_delay_bark_left - min(group_delay_bark_left);
+    [trueOct_left, trueOct_f]=trueOctave(db_left, freq);
     
     [mag_bark_right, freq_bark] = rlogbark_short(freq, db_right);
     [phase_bark_right, freq_bark] = rgeobark_short(freq, myphase_right);
     [group_delay_bark_right, freq_bark] = rgeobark_short(freq, group_delay_right);
     group_delay_bark_right = group_delay_bark_right - min(group_delay_bark_right);
-    
+    [trueOct_right, trueOct_f]=trueOctave(db_right, freq);
 end
 
     
@@ -169,6 +130,8 @@ end
      assignin('base','mag_bark_left',mag_bark_left);
      assignin('base', 'k10', k10);
      assignin('base', 'fcenter10', fcenter10);
+     assignin('base', 'trueOct_f', trueOct_f);
+     assignin('base', 'trueOct_right', trueOct_right);
      
      assignin('base','mOrs',mOrs);
      
@@ -183,28 +146,32 @@ end
      assignin('base','mag_bark_right',mag_bark_right);
      assignin('base', 'k10', k10);
      assignin('base', 'fcenter10', fcenter10);
+     assignin('base', 'trueOct_f', trueOct_f);
+     assignin('base', 'trueOct_left', trueOct_left);
      
      assignin('base','mOrs',mOrs);
    else 
      assignin('base','freq',freq);
-     assignin('base','group_delay_left',group_delay_left);
+     assignin('base', 'trueOct_f', trueOct_f);
      assignin('base','freq_bark',freq_bark);
+     
+     assignin('base','group_delay_left',group_delay_left);
      assignin('base','group_delay_bark_left',group_delay_bark_left);
      assignin('base','myphase_left',myphase_left);
      assignin('base','phase_bark_left',phase_bark_left);
      assignin('base','db_left',db_left);
      assignin('base','mag_bark_left',mag_bark_left);
+     assignin('base', 'trueOct_left', trueOct_left);
      
      assignin('base','mOrs',mOrs);
      
-     assignin('base','freq',freq);
      assignin('base','group_delay_right',group_delay_right);
-     assignin('base','freq_bark',freq_bark);
      assignin('base','group_delay_bark_right',group_delay_bark_right);
      assignin('base','myphase_right',myphase_right);
      assignin('base','phase_bark_right',phase_bark_right);
      assignin('base','db_right',db_right);
      assignin('base','mag_bark_right',mag_bark_right);
+     assignin('base', 'trueOct_right', trueOct_right);
      
      assignin('base', 'k10', k10);
      assignin('base', 'fcenter10', fcenter10);
---
+++ AppConversion/trueOctave.m
@@ -0,0 +1,15 @@
+% Calculated the True Octave smooth of a signal
+% Input: amp (signal), f (frequency)
+function [filt_sig,fcenter] = trueOctave(amp, f)
+
+    fcenter = (10.^(0.3*[4:15]));
+    fd = 2^0.5;
+    fupper = fcenter * fd;
+    flower = fcenter / fd;
+    filt_sig = zeros(length(fcenter), 1);
+    for i=1:length(fcenter)
+        filt_sig(i) =  10*log10(sum(10.^(0.1*amp(find(f >= flower(i) & f <= fupper(i)))))/length(find(f >= flower(i) & f <= fupper(i)))); % logarithmic mean
+    end
+
+end
+
